e5b9245040191261284783513a47e872a0bf34de
arvindra1 <arvindra.ahirwar@djtcorp.in>
2025-11-18 18:12:38 +0530

    inm

--- android/src/main/java/video/api/reactnative/livestream/utils/OrientationManager.kt
+++ android/src/main/java/video/api/reactnative/livestream/utils/OrientationManager.kt
@@ -1,48 +1,29 @@
-package video.api.reactnative.livestream.utils
-
-import android.content.Context
-import android.view.OrientationEventListener
-import java.io.Closeable
-
-class OrientationManager(context: Context) : Closeable {
-  private val orientationEventListener = object : OrientationEventListener(context) {
-    override fun onOrientationChanged(orientation: Int) {
-      currentOrientation = (orientation + 45) / 90 * 90 % 360
+// This code from your previous implementation will now work smoothly
+React.useEffect(() => {
+  const handleOrientationChange = (orientation: string) => {
+    let newOrientation: 'PORTRAIT' | 'LANDSCAPE' = 'PORTRAIT';
+    
+    if (orientation === 'LANDSCAPE-LEFT' || orientation === 'LANDSCAPE-RIGHT') {
+      newOrientation = 'LANDSCAPE';
+    } else if (orientation === 'PORTRAIT' || orientation === 'PORTRAIT-UPSIDEDOWN') {
+      newOrientation = 'PORTRAIT';
     }
-  }.apply {
-    enable()
-  }
-
-  private var currentOrientation: Int = OrientationEventListener.ORIENTATION_UNKNOWN
-    set(value) {
-      if (field != value) {
-        if (field == OrientationEventListener.ORIENTATION_UNKNOWN) {
-          // First time we get an orientation value, don't consider it as a change
-          field = value
-          return
-        } else {
-          field = value
-          _orientationHasChanged = true
-        }
-      }
+    
+    console.log(`📱 Orientation changed to: ${newOrientation}`);
+    setCurrentOrientation(newOrientation);
+    
+    // Stream will NOT disconnect anymore! ✅
+    if (streaming && socketConnected && playbackid) {
+      socketRef.current?.emit('broadcaster-orientation', {
+        playbackId: playbackid,
+        orientation: newOrientation,
+      });
     }
+  };
 
-
-  private var _orientationHasChanged: Boolean = false
-
-  val orientationHasChanged: Boolean
-    get() {
-      if (!orientationEventListener.canDetectOrientation()) {
-        // Assume orientation has changed if we can't detect it
-        return true
-      }
-
-      val orientationHasChanged = _orientationHasChanged
-      _orientationHasChanged = false
-      return orientationHasChanged
-    }
-
-  override fun close() {
-    orientationEventListener.disable()
-  }
-}
+  Orientation.addOrientationListener(handleOrientationChange);
+  
+  return () => {
+    Orientation.removeOrientationListener(handleOrientationChange);
+  };
+}, [streaming, socketConnected, playbackid]);
